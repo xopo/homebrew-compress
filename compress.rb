a7bd66459deed53155d7dd86248b3bfbd0d3ba84
opo <opo@local>
2026-02-17 13:52:04 +0200

    - v0.5beta

--- compress.rb
+++ compress.rb
@@ -1,9 +1,9 @@
 class Compress < Formula
   desc "Compress is a file watcher that monitors ~/Desktop/screenshots for *.mov files and converts it to *.mp4 using Ffmpeg"
   homepage "https://github.com/xopo/compress"
-  version "v0.2.beta"
-  url "https://github.com/xopo/compress/releases/download/beta/compress.tar.gz"
-  sha256 "4bf1bf13b51f4bc96d9e431ca8ef7a217871797eb195e2424b6f1f5034763701"
+  version "v0.5beta"
+  url "https://github.com/xopo/compress/releases/download/v0.5beta/compress.tar.gz"
+  sha256 "37eb3afe6902c9d7a2b1095ad18ccc72a4c6a8568d174c7ea2a298c17c6a46fe"
   license "MIT"
 
   def install
